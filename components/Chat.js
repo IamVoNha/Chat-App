5c4c92ba30f03e8e5426c59e499afe372330164e
IamVoNha <ov_ahn@yahoo.com>
2021-11-26 04:51:17 -0500

    Implemented Gifted Chat - Task 5.2
    5.2: Chat UIs & Accessibility

--- components/Chat.js
+++ components/Chat.js
@@ -1,28 +1,76 @@
 import React from 'react';
- import { View, Text } from 'react-native';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
  export default class Chat extends React.Component {
+ 	state = {
+ 		messages: []
+ 	};
+
+ 	componentDidMount() {
+ 		let name = this.props.route.params.name;
+ 		this.setState({
+ 			messages: [
+ 				{
+ 					_id: 1,
+ 					text: 'Hello developer',
+ 					createdAt: new Date(),
+ 					user: {
+ 						_id: 2,
+ 						name: 'React Native',
+ 						avatar: 'https://placeimg.com/140/140/any',
+ 					},
+ 				},
+ 				{
+ 					_id: 2,
+ 					text: name +` has entered the chat.`,
+ 					createdAt: new Date(),
+ 					system: true,
+ 				}
+ 			]
+ 		});
+ 	}
+
+ 	// This appends the new message to the messages object.
+ 	onSend = (messages = []) => {
+ 		this.setState((previousState) => ({
+ 			messages: GiftedChat.append(previousState.messages, messages)
+ 		}));
+ 	};
+
+ 	// Customizing the right bubble message color to blue!
+ 	renderBubble(props) {
+ 		return (
+ 			<Bubble
+ 				{...props}
+ 				wrapperStyle={{
+ 					right: {
+ 						backgroundColor: '#0096FF'
+ 					}
+ 				}}
+ 			/>
+ 		)
+ 	}
+
  	render() {
  		let name = this.props.route.params.name;
  		let background = this.props.route.params.background;
-
- 		this.props.navigation.setOptions({ title: name });
-
- 		return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: background
-        }}
-      >
-        {name ? (
-          <Text style={{ color: '#fff' }}>Hello, {name}</Text>
-        ) : (
-        <Text style={{ color: '#fff' }}>Please state your name!</Text>
-        )}
-    </View>
- 		);
- 	}
- }
+    // Sets the entered name as the title in the Chat screen
+		this.props.navigation.setOptions({ title: name });
+		return (
+ 			<View style={{ flex: 1, backgroundColor: background }}>
+ 				<GiftedChat
+            //change the color of the chat bubble
+            renderBubble={this.renderBubble.bind(this)}
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+        />
+          {Platform.OS === 'android' ? 
+          <KeyboardAvoidingView behavior="height" /> : null}
+      </View>
+    )
+  }
+}

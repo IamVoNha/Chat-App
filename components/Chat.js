fe9cca628ab784128cd48ecda8861c8ed3f7f7e0
IamVoNha <ov_ahn@yahoo.com>
2021-11-30 05:09:06 -0500

    Task 5.3 - Firestore
    Real-Time Applications & Data Storage

--- components/Chat.js
+++ components/Chat.js
@@ -2,41 +2,90 @@ import React from 'react';
 import { View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
  export default class Chat extends React.Component {
- 	state = {
+  constructor() {
+    super();
+
+  if(!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBPuELOaVfUx4LUCD5k9Kab-8uFbhm_3l8",
+      authDomain: "chatapp-f1831.firebaseapp.com",
+      projectId: "chatapp-f1831",
+      storageBucket: "chatapp-f1831.appspot.com",
+      messagingSenderId: "27418270029",
+      appId: "1:27418270029:web:2cc850b5e2f61aff3bbf04"
+    });
+  }
+
+  this.referenceChatMessages = firebase.firestore().collection('messages');
+
+ 	this.state = {
  		messages: []
  	};
+}
 
  	componentDidMount() {
  		let name = this.props.route.params.name;
- 		this.setState({
- 			messages: [
- 				{
- 					_id: 1,
- 					text: 'Hello developer',
- 					createdAt: new Date(),
- 					user: {
- 						_id: 2,
- 						name: 'React Native',
- 						avatar: 'https://placeimg.com/140/140/any',
- 					},
- 				},
- 				{
- 					_id: 2,
- 					text: name +` has entered the chat.`,
- 					createdAt: new Date(),
- 					system: true,
- 				}
- 			]
- 		});
- 	}
+
+     this.props.navigation.setOptions({ title: name });
+
+     this.authUnsubscribe = firebase.auth().onAuthStateChanged((user)=> {
+       if(!user) {
+         firebase.auth().signInAnonymously();
+       }
+       this.setState({
+        uid:user.uid,
+        messages:[]
+      });
+      this.unsubscribe=this.referenceChatMessages
+      .orderBy('createdAt','desc')
+      .onSnapshot(this.onCollectionUpdate);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    querySnapshot.forEach((doc) => {
+      let data = doc.data(); 
+      messages.push({
+        _id:data._id,
+        text:data.text,
+        createdAt:data.createdAt.toDate(),
+        user:data.user,
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+  addMessage() {
+    const message=this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id:message._id,
+      text:message.text,
+      createdAt:message.createdAt,
+      user:message.user
+    });
+  }
 
  	// This appends the new message to the messages object.
- 	onSend = (messages = []) => {
- 		this.setState((previousState) => ({
- 			messages: GiftedChat.append(previousState.messages, messages)
- 		}));
- 	};
+ 	onSend(messages=[]) {
+    this.setState(
+      (previousState)=>({
+        messages:GiftedChat.append(previousState.messages, messages),
+      }),
+    ()=>{
+      this.addMessage();
+      }
+    );
+  }
 
  	// Customizing the right bubble message color to blue!
  	renderBubble(props) {
